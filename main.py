02cc00a0d5ff7a933a2bf830866aed99727afd3e
Varshith8868 <konalavasrshith@gmail.com>
2025-08-11 21:03:18 +0530

    commit

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutr
 face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Start webcam
-cap=cv2.VideoCapture(0)
+cap =cv2.VideoCapture(0)
 
 while True:
 
-    ret, frame = cap.read()
+    ret, frame=cap.read()
     if not ret:
         break
 
