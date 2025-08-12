ca0a29b99ecb2621ba3dd09ec751dab2aec7f0bb
Varshith8868 <konalavasrshith@gmail.com>
2025-08-12 10:11:47 +0530

    d

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from tensorflow.keras.preprocessing.image import img_to_array
 model = load_model ('model/emotion_model.h5', compile=False)
 
 # Emotion labels based on FER-2013
-emotion_labels = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 # Load OpenCV's Haar cascade face detector
 
-face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 # Start webcam
 cap =cv2.VideoCapture(0)
 
 while True:
 
-    ret, frame=cap.read()
+    ret, frame=cap.read() 
     if not ret:
         break
 
