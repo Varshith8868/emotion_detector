bd38b6baf8f867a8d8f0917560043a2e00d05634
Varshith8868 <konalavasrshith@gmail.com>
2025-08-20 21:27:20 +0530

    w

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ emotion_labels = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
 # Load OpenCV's Haar cascade face detector
 
 
-face_classifier=cv2.CascadeClassifier (cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Start webcam 
 cap = cv2.VideoCapture(0)
