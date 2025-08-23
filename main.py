4bf5a036040815a1ae7288c7c374f30f8a33b905
Varshith8868 <konalavasrshith@gmail.com>
2025-08-23 10:31:41 +0530

    asc

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ cap = cv2.VideoCapture(0)
 while True:
 
     ret,frame= cap.read() 
-    if not ret:
+    if not ret :
         break;
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_classifier.detectMultiScale(gray, 1.3, 5)
