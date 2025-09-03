ae9d11e0ff88320b5857b56715a402a6fa2242c6
Varshith8868 <konalavasrshith@gmail.com>
2025-09-03 22:24:53 +0530

    ks

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ emotion_labels = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
 # Load OpenCV's Haar cascade face detector
 face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # Start webcam 
-cap = cv2.VideoCapture(0) 
+
+cap=cv2.VideoCapture(0) 
 while True:
     ret,frame= cap.read() 
     if not ret:
-        
+
         break;
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_classifier.detectMultiScale(gray, 1.3, 5)
