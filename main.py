b2e2e07b9d056422cf321c312fb545e75bed20ca
Varshith8868 <konalavasrshith@gmail.com>
2025-09-08 19:34:45 +0530

    e

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
 # Load the pre-trained model
-model = load_model('model/emotion_model.h5', compile=False)
+model=load_model('model/emotion_model.h5', compile=False)
 
 # Emotion labels based on FER-2013
 emotion_labels = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral']
